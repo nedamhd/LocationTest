9281d5f772575fd50f905bc8da3ac2d0481cc825
Neda mohammadi <n.mohammadi.24@gmail.com>
2024-11-21 23:35:55 +0330

    Update LocationTest.R

--- R/LocationTest.R
+++ R/LocationTest.R
@@ -1,13 +1,19 @@
-Location.Test = function(data, var,group, Test=NULL, draw_plot=TRUE, save_plot=TRUE, y_adjust=1.2,filename= "plot.123",...) {
-require(dplyr)
-require(ggplot2)  
-data$.group123 = data[[group]]
+Location.Test = function (data, var, group, Test = NULL, draw_plot = TRUE, save_plot = TRUE, 
+          y_adjust = 1.2, filename = "plot.123", ...) 
+{
+  require(dplyr)
+  require(ggplot2)
+  data$.group123 = data[[group]]
   uniq.level = unique(data$.group123)
-  if (length(uniq.level)==2)
-    {Test0=Draw.table (data, var, group,Test)}
-  if (length(uniq.level)>2)
-    {Test0=Anova.Table(data, group , var, Test ) }
-
+  if (length(uniq.level) == 2) {
+    Test0 = Draw.table(data, var, group, Test,)
+  }
+  if (length(uniq.level) > 2) {
+    Test0 = Anova.Table(data, group, var, Test, draw_plot = draw_plot, save_plot = save_plot, 
+    y_adjust = y_adjust, filename = filename, ...)   
+    
+    
+  }
   Test0
 }
 
