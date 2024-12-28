e2a087c634ae3ebba8914eac8df4032bc49331ce
Neda mohammadi <n.mohammadi.24@gmail.com>
2024-12-28 23:00:20 +0330

    Update LocationTest.R

--- R/LocationTest.R
+++ R/LocationTest.R
@@ -1,11 +1,18 @@
-Location.Test = function (data, var, group, Test = NULL, paired=FALSE, paired.var=NULL, draw_plot = TRUE, save_plot = TRUE, 
+Location.Test = function (data, data, group=NULL, Test = NULL, paired=FALSE, paired.var=NULL, draw_plot = TRUE, save_plot = TRUE, 
           y_adjust = 1.2, filename = "plot.123", ...) 
 {
+          
   require(dplyr)
   require(ggplot2)
+  if(paired){        
+  var.value = data[,var]
+  paired.var.value = data[,paired.var]
+  data = data.frame(group = 
+            
   data$.group123 = data[[group]]
   uniq.level = unique(data$.group123)
   if (length(uniq.level) == 2) {
+            
     Test0 = Draw.table(data, var, group, Test, paired=paired, paired.var=paired.var)
   }
   if (length(uniq.level) > 2) {
