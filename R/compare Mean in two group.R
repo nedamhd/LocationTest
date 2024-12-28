f49f48431591cb2b3b5ea2e1695e1054b3c15eec
Neda mohammadi <n.mohammadi.24@gmail.com>
2024-12-28 22:36:32 +0330

    Update compare Mean in two group.R

--- R/compare Mean in two group.R
+++ R/compare Mean in two group.R
@@ -1,19 +1,24 @@
-
-
-##########################
-Test_Mean = function(data, var, group,Test=NULL){
- formula=as.formula(paste0(var,"~",group))
+Test_Mean = function(data, var, group = NULL,Test=NULL, paired=FALSE){
+if(isFALSE(paired)){
+    formula=as.formula(paste0(var,"~",group))
   R=Check_normality(data=data,var=var)
-  if (is.null(Test)){
-  if (R$p.value<0.05) {
-    p.value = wilcox.test(formula, data=data)$p.value
-    Test = "wilcox"
-  }
-  else{
-    p.value = t.test(formula, data=data)$p.value
-    Test = "t.test"
-  }}
+} else {
+  formula=as.formula(paste0("Pair(",paste0(var, collapse = ","),")~",1))
+  newdata = data.frame(diffvar = data[,var[1]] - data[,var[2]] )
+  R=Check_normality(data=newdata,var="diffvar")
+  
+}
 
+  if (is.null(Test)){
+    if (R$p.value<0.05) {
+      p.value = wilcox.test(formula, data=data)$p.value
+      Test = "wilcox"
+    }
+    else{
+      p.value = t.test(formula, data=data)$p.value
+      Test = "t.test"
+    }}
+  
   if (!is.null(Test)){
     if (Test=="wilcox") {
       p.value = wilcox.test(formula, data=data)$p.value
@@ -24,7 +29,9 @@ Test_Mean = function(data, var, group,Test=NULL){
       Test = "t.test"
     }}
 
-
+  
+  
+  
   data.frame(p.value, Test)
-
+  
 }
